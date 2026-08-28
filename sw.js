bebff2c71d949bb2cdebbc85b046cd9c472fb4a7
cas82640 <n.matsuzaki1974@gmail.com>
2026-08-28 14:00:39 +0900

    Publish iOS image restore v0.9.4

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'photo-recipe-lab-v16';
+const CACHE_NAME = 'photo-recipe-lab-v17';
 const APP_SHELL = [
   "./",
   "./assets/index-BuyFDHwd.css",
-  "./assets/index-Cjbzy1Er.js",
+  "./assets/index-fbXzILtG.js",
   "./icons/icon-maskable.svg",
   "./icons/icon.svg",
   "./index.html",
